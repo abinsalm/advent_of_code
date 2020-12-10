9933b7c6d0a8f04369ea641187efc96dcecdbd12
abdullah bin salm <abinsalm@sonatype.com>
2020-12-09 23:36:12 -0700

    day 10: add top down dp approach

--- 2020/day_10/part_2.py
+++ 2020/day_10/part_2.py
@@ -11,12 +11,33 @@ numbers.sort()
 last_number = max(numbers) + 3
 numbers = [0] + numbers + [last_number]
 
-dp = [0] * len(numbers)
-dp[0] = 1
-for i in range(1, len(numbers)):
-    for j in range(i - 1, -1, -1):
-        curr_num = numbers[i]
-        prev = numbers[j]
-        if curr_num - prev > 3: break
-        dp[i] += dp[j]
-print(dp[-1])
+comb = set()
+
+# TOP DOWN
+from functools import lru_cache
+
+
+@lru_cache(None)
+def backtrack(curr, index):
+    if index == len(numbers):
+        return 1
+    comb = 0
+    while index < len(numbers) and numbers[index] - curr <= 3:
+        comb += backtrack(numbers[index], index + 1)
+        index += 1
+
+    return comb
+
+
+print('comb: ', backtrack(0, 1))
+
+# BOTTOM UP
+# dp = [0] * len(numbers)
+# dp[0] = 1
+# for i in range(1, len(numbers)):
+#     for j in range(i-1, -1, -1):
+#         curr_num = numbers[i]
+#         prev = numbers[j]
+#         if curr_num - prev > 3: break
+#         dp[i] += dp[j]
+# print(dp[-1])
