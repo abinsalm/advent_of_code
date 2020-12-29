bc0001ac560f95dec9f6985659a4eac905200c03
abdullah bin salm <abinsalm@sonatype.com>
2020-12-29 13:48:54 -0700

    day 20:  WIP

--- 2020/day_20/part_1.py
+++ 2020/day_20/part_1.py
@@ -1,7 +1,12 @@
 filename = 'part_1.txt'
 
-roles = {}
-process_roles = True
-possible_solutions = set()
-
-
+images = []
+with open(filename, encoding="utf-8") as f:
+    lines = f.read().split("\n\n")
+    for line in lines:
+        line = line.split('\n')
+        id = line[0].split(' ')[1][:-1]
+        data = line[1:]
+        borders = data[0], ''.join([data[i][-1] for i in range(10)]), data[-1], ''.join([data[i][0] for i in range(10)])
+        # print(id, borders, data)
+        images.append((id, borders))
